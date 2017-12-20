552a3ad2d52cb7fcf4123b7487221740acb1bbbc
Marcel Eglseder <marcel.eglseder@web.de>
2017-12-20 11:08:31 +0100

    Initial commit

---
+++ src/com/sabel/AlgorithmenDatenstrukturen/Quersumme.java
@@ -0,0 +1,14 @@
+package com.sabel.AlgorithmenDatenstrukturen;
+
+public class Quersumme {
+    private int zahl, quersumme;
+
+    public Quersumme(int zahl) {
+        this.zahl = zahl;
+    }
+
+    public int quersummeBerechnen(int zahl) {
+
+        return 0;
+    }
+}
